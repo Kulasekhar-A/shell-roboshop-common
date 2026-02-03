197e354d64cd32cd2943ff7cdbb013bae2828b40
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-03 19:28:57 +0530

    actions

---
+++ catalogue.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+app_setup
+nodejs_setup
+systemd_setup
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-mongosh -y &>> $LOGS_FILE
+VALIDATE $? "Install mongodb"
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")' )
+if [ $INDEX -le 0 ]; then
+   mongosh --host $MONGODB_HOST </app/db/master-data.js
+   VALIDATE $? "Loading products"
+else
+   echo -e "Products already loaded ... $Y skipping $N"
+fi
+
+print_total_time
--- common.sh
+++ common.sh
@@ -9,6 +9,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 mkdir -p  $LOGS_FOLDER
+SCRIPT_DIR=$PWD
+MONGODB_HOST="mongodb.annuru.online"
 START_TIME=$(date +%s)
 
 echo "$(date "+%y-%m-%d %H:%M:%S") | script start executing at : $(date)" | tee -a $LOGS_FILE
@@ -30,6 +32,64 @@ VALIDATE(){
    fi
 }
 
+nodejs_setup(){
+    dnf module disable nodejs -y &>> $LOGS_FILE
+    VALIDATE $? "Disabling Nodejs default version"
+
+    dnf module enable nodejs:20 -y &>> $LOGS_FILE
+    VALIDATE $? "Enable version"
+
+    dnf install nodejs -y &>> $LOGS_FILE
+    VALIDATE $? "Install Nodejs"
+
+    npm install &>> $LOGS_FILE
+    VALIDATE $? "Installing NPM"
+}
+
+app_setup(){
+        id roboshop &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
+        VALIDATE $> "Creating system user"
+    else
+    echo -e "Already exist ... $Y Skipping it $N"
+
+    fi
+
+    mkdir -p /app &>> $LOGS_FILE
+    VALIDATE $? "App directory"
+
+    curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> $LOGS_FILE
+    VALIDATE $? "Download the code"
+
+    cd /app &>> $LOGS_FILE
+    VALIDATE $? "Moving to app directory"
+
+    rm -rf /app/*
+    VALIDATE $? "Removing existing code"
+
+    unzip /tmp/catalogue.zip &>> $LOGS_FILE
+    VALIDATE $? "Unzip the code"
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+    VALIDATE $? "Copying catalogue service"
+
+    systemctl daemon-reload &>> $LOGS_FILE
+    VALIDATE $? "Reload the service"
+
+    systemctl enable catalogue &>> $LOGS_FILE
+    VALIDATE $? "Enable the catalogue service"
+
+    systemctl start catalogue &>> $LOGS_FILE
+    VALIDATE $? "Start the catalogue service"
+}
+
+restart_setup(){
+    systemctl restart catalogue &>> $LOGS_FILE
+    VALIDATE $? "Restart the catalogue"
+}
 print_total_time(){
 
 END_TIME=$(date +%s)
