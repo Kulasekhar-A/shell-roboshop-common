a927448a442f1c706af72f312943e460c2ee228e
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-03 19:11:11 +0530

    actions

---
+++ mongodb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying mongo repo"
+
+dnf install mongodb-org -y &>> $LOGS_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>> $LOGS_FILE
+VALIDATE $? "Enable Mongod"
+
+systemctl start mongod &>> $LOGS_FILE
+VALIDATE $? "Start Mongod"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl restart mongod &>> $LOGS_FILE
+VALIDATE $? "Restart Mongod"
+
+print_total_time
