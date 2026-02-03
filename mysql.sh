2365bd723ebcab2b560be9b1f0babd114cf56581
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-03 21:48:26 +0530

    actions

---
+++ mysql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=redis
+
+check_root
+
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing Mysql...."
+
+systemctl enable mysqld &>> $LOGS_FILE
+VALIDATE $?  "Enable mysqld"
+
+systemctl start mysqld &>> $LOGS_FILE
+VALIDATE $? "Start mysqld"
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>> $LOGS_FILE
+VALIDATE $? "set root password for mysql"
+
+print_total_time
