f43f47fec24aeb6284fa9c497924695be207b448
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-04 12:45:39 +0530

    actions

--- frontend.sh
+++ frontend.sh
@@ -35,6 +35,6 @@ VALIDATE $? "Unzip the code"
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>> $LOGS_FILE
 VALIDATE $? "copying nginx configure" 
 
-restart_setup
+restart_setu
 
 print_total_time
